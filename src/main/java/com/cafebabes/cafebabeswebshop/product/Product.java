6f7aed0d8345d2ddd23a39a99a3004874f06d1b0
gkevin94 <gkevin94@gmail.com>
2022-04-19 20:52:54 +0200

    created Product.java with attributes

---
+++ src/main/java/com/cafebabes/cafebabeswebshop/product/Product.java
@@ -0,0 +1,13 @@
+package com.cafebabes.cafebabeswebshop.product;
+
+public class Product {
+
+    private long id;
+    private String code;
+    private String address;
+    private String name;
+    private String manufacture;
+    private int price;
+    private String productStatus;
+
+}
