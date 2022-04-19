7d082e4974cfd80a9b8f7ba8a7257aea29efc261
gkevin94 <gkevin94@gmail.com>
2022-04-19 20:54:02 +0200

    generated contructors in Product.java

--- src/main/java/com/cafebabes/cafebabeswebshop/product/Product.java
+++ src/main/java/com/cafebabes/cafebabeswebshop/product/Product.java
@@ -10,4 +10,27 @@ public class Product {
     private int price;
     private String productStatus;
 
+    public Product() {
+    }
+
+    public Product(String address, String name, String manufacture, int price) {
+        this.address = address;
+        this.name = name;
+        this.manufacture = manufacture;
+        this.price = price;
+    }
+
+    public Product(String address, String name, String manufacture, int price, String productStatus) {
+        this(address, name, manufacture, price);
+        this.productStatus = productStatus;
+    }
+
+    public Product(long id, String code, String address, String name, String manufacture, int price, String productStatus/*, Category category*/) {
+        this(address, name, manufacture, price);
+        this.id = id;
+        this.code = code;
+        this.productStatus = productStatus;
+        //this.category = category;
+    }
+
 }
