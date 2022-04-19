ebd675601ef47a1a2ff7e1cb28c4cda9b97d1a35
gkevin94 <gkevin94@gmail.com>
2022-04-19 20:54:33 +0200

    generated getters and setters in Product.java

--- src/main/java/com/cafebabes/cafebabeswebshop/product/Product.java
+++ src/main/java/com/cafebabes/cafebabeswebshop/product/Product.java
@@ -33,4 +33,68 @@ public class Product {
         //this.category = category;
     }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getManufacture() {
+        return manufacture;
+    }
+
+    public void setManufacture(String manufacture) {
+        this.manufacture = manufacture;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getProductStatus() {
+        return productStatus;
+    }
+
+    public void setProductStatus(String productStatus) {
+        this.productStatus = productStatus;
+    }
+
+    /*public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }*/
+
 }
