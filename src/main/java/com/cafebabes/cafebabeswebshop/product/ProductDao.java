ca218c363a784ee01fd1c378ea4303fdac0f722a
gkevin94 <gkevin94@gmail.com>
2022-04-25 12:15:17 +0200

    implemented getProduct method in ProductDao.java

--- src/main/java/com/cafebabes/cafebabeswebshop/product/ProductDao.java
+++ src/main/java/com/cafebabes/cafebabeswebshop/product/ProductDao.java
@@ -1,10 +1,14 @@
 package com.cafebabes.cafebabeswebshop.product;
 
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ProductDao {
+
+    private JdbcTemplate jdbcTemplate;
 
     private static final RowMapper<Product> PRODUCT_ROW_MAPPER = ((resultSet, i) -> new Product(
             resultSet.getLong("id"),
@@ -19,4 +23,18 @@ public class ProductDao {
                     resultSet.getString("category.name"),
                     resultSet.getLong("category.ordinal"))
     ));
+
+    private static final String SQL_SELECT_ALL_JOIN_CATEGORY =
+            "SELECT products.id, code, address, products.name, manufacture, price, product_status, category_id, category.name, category.ordinal " +
+                    "FROM products LEFT JOIN category ON category_id=category.id ";
+
+    public Product getProduct(String address) {
+        try {
+            return jdbcTemplate.queryForObject( SQL_SELECT_ALL_JOIN_CATEGORY+
+                            "WHERE address = ?",
+                    PRODUCT_ROW_MAPPER, address);
+        } catch (EmptyResultDataAccessException e) {
+            throw new IllegalStateException();
+        }
+    }
 }
