68887bf10dcf7aad89c086fcba94978ff1e55414
gkevin94 <gkevin94@gmail.com>
2022-04-25 12:49:05 +0200

    implemented isValid method in ProductValidator.java

--- src/main/java/com/cafebabes/cafebabeswebshop/product/ProductValidator.java
+++ src/main/java/com/cafebabes/cafebabeswebshop/product/ProductValidator.java
@@ -1,4 +1,8 @@
 package com.cafebabes.cafebabeswebshop.product;
 
 public class ProductValidator {
+
+    public boolean isValid(String str) {
+        return (str != null && !str.trim().equals(""));
+    }
 }
