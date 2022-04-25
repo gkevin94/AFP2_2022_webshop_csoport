f3f2ce9f54655bef70596e55c58acb8301f22164
gkevin94 <gkevin94@gmail.com>
2022-04-25 12:36:40 +0200

    implemented getProducts method in ProductService.java

--- src/main/java/com/cafebabes/cafebabeswebshop/product/ProductService.java
+++ src/main/java/com/cafebabes/cafebabeswebshop/product/ProductService.java
@@ -2,6 +2,19 @@ package com.cafebabes.cafebabeswebshop.product;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ProductService {
+
+    private ProductDao productDao;
+    //private CategoryDao categoryDao;
+
+    public Product getProduct(String address) {
+        return productDao.getProduct(address);
+    }
+
+    public List<Product> getProducts() {
+        return productDao.getProducts();
+    }
 }
