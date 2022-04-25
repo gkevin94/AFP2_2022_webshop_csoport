2b8511df6b42b5b79524b6db36309f764cfd9cc8
gkevin94 <gkevin94@gmail.com>
2022-04-25 12:12:05 +0200

    created PRODUCT_ROW_MAPPER as static attribute in ProductDao.java

--- src/main/java/com/cafebabes/cafebabeswebshop/product/ProductDao.java
+++ src/main/java/com/cafebabes/cafebabeswebshop/product/ProductDao.java
@@ -1,7 +1,22 @@
 package com.cafebabes.cafebabeswebshop.product;
 
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ProductDao {
+
+    private static final RowMapper<Product> PRODUCT_ROW_MAPPER = ((resultSet, i) -> new Product(
+            resultSet.getLong("id"),
+            resultSet.getString("code"),
+            resultSet.getString("address"),
+            resultSet.getString("name"),
+            resultSet.getString("manufacture"),
+            resultSet.getInt("price"),
+            resultSet.getString("product_status"),
+            new Category(
+                    resultSet.getLong("category_id"),
+                    resultSet.getString("category.name"),
+                    resultSet.getLong("category.ordinal"))
+    ));
 }
