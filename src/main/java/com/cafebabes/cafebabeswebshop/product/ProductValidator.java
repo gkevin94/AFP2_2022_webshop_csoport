669b7c744d5fe3ffe30104ea742d5a46cc4063f3
gkevin94 <gkevin94@gmail.com>
2022-04-25 12:57:15 +0200

    implemented isValidProduct and isValidPrice in ProductValidator.java

--- src/main/java/com/cafebabes/cafebabeswebshop/product/ProductValidator.java
+++ src/main/java/com/cafebabes/cafebabeswebshop/product/ProductValidator.java
@@ -5,4 +5,17 @@ public class ProductValidator {
     public boolean isValid(String str) {
         return (str != null && !str.trim().equals(""));
     }
+
+    public boolean isValidProduct(Product product){
+        return isValid(product.getCode()) &&
+                isValid(product.getName()) &&
+                //isValid(product.getAddress()) &&
+                isValid(product.getManufacture()) &&
+                isValidPrice(product.getPrice()) &&
+                isValid(product.getCategory().getName());
+    }
+
+    public boolean isValidPrice(int price){
+        return (price > 0 && price <= 2_000_000);
+    }
 }
