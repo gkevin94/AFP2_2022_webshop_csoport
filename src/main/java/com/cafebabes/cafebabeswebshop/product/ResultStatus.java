684705fd6cebfb00a184d0b1b0546029114c539f
gkevin94 <gkevin94@gmail.com>
2022-04-25 12:46:40 +0200

    added 3 attributes to ResultStatus

--- src/main/java/com/cafebabes/cafebabeswebshop/product/ResultStatus.java
+++ src/main/java/com/cafebabes/cafebabeswebshop/product/ResultStatus.java
@@ -1,5 +1,8 @@
 package com.cafebabes.cafebabeswebshop.product;
 
-public class ResultStatus {
+public class ResultStatus<T> {
 
+    private T type;
+    private ResultStatusEnum status;
+    private String message;
 }
