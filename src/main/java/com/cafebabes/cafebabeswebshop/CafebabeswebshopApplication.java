9fdf924a768ebaa05cc584115f956f07dd52d68d
gkevin94 <gkevin94@gmail.com>
2022-04-25 11:04:33 +0200

    override configure method from WebSecurityConfigurerAdapter and set configuration

--- src/main/java/com/cafebabes/cafebabeswebshop/CafebabeswebshopApplication.java
+++ src/main/java/com/cafebabes/cafebabeswebshop/CafebabeswebshopApplication.java
@@ -3,6 +3,8 @@ package com.cafebabes.cafebabeswebshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -16,5 +18,20 @@ public class CafebabeswebshopApplication extends WebSecurityConfigurerAdapter {
 		SpringApplication.run(CafebabeswebshopApplication.class, args);
 	}
 
-
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.csrf().disable()
+				.authorizeRequests()
+				.antMatchers("/", "/js/**", "/css/**").permitAll()
+				.antMatchers("/basket.html", "/myorders.html").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+				.antMatchers("/adminproducts.html", "/adminusers.html", "/users.html", "/dashboard.html", "/reports.html", "/orders.html", "/orders", "/reports", "/dashboard", "/categories.html", "/upload.html").hasRole("ADMIN")
+				.antMatchers(HttpMethod.GET, "/users").hasRole("ADMIN")
+				.and()
+				.formLogin()
+				.loginPage("/login.html").loginProcessingUrl("/login")
+				.defaultSuccessUrl("/index.html")
+				.and()
+				.logout();
+	}
 }
