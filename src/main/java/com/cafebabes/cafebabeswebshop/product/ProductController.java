9ec6913e8094e3a2e972f509ad4448eba91ec725
gkevin94 <gkevin94@gmail.com>
2022-04-25 12:52:27 +0200

    implemented getProduct for "/product/{address}" endpoint in ProductController.java

--- src/main/java/com/cafebabes/cafebabeswebshop/product/ProductController.java
+++ src/main/java/com/cafebabes/cafebabeswebshop/product/ProductController.java
@@ -1,6 +1,11 @@
 package com.cafebabes.cafebabeswebshop.product;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class ProductController {
@@ -11,5 +16,15 @@ public class ProductController {
     public ProductController(ProductService productService) {
         this.productService = productService;
         this.validator = new ProductValidator();
+    }
+
+    @GetMapping("/product/{address}")
+    public Object getProduct(@PathVariable String address) {
+        List<String> addresses = productService.getProducts().stream().map(Product::getAddress).collect(Collectors.toList());
+        if (validator.isValid(address) && addresses.contains(address)) {
+            return productService.getProduct(address);
+        } else {
+            return new ResultStatus(ResultStatusEnum.NOT_OK, "Invalid address");
+        }
     }
 }
