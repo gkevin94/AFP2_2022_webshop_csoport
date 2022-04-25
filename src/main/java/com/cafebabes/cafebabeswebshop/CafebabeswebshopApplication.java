ea978f7348f51765aecb4ceac95b15617ee0245e
gkevin94 <gkevin94@gmail.com>
2022-04-25 11:57:34 +0200

    implemented configureGlobal as Autowired for authentication

--- src/main/java/com/cafebabes/cafebabeswebshop/CafebabeswebshopApplication.java
+++ src/main/java/com/cafebabes/cafebabeswebshop/CafebabeswebshopApplication.java
@@ -1,15 +1,19 @@
 package com.cafebabes.cafebabeswebshop;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableWebSecurity
@@ -41,5 +45,12 @@ public class CafebabeswebshopApplication extends WebSecurityConfigurerAdapter {
 	@Bean
 	public PasswordEncoder encoder() {
 		return new BCryptPasswordEncoder(4);
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth, DataSource dataSource, PasswordEncoder passwordEncoder) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(passwordEncoder)
+				.usersByUsernameQuery("SELECT user_name,password,enabled FROM users WHERE user_name=?")
+				.authoritiesByUsernameQuery("SELECT user_name, role FROM users WHERE user_name = ?");
 	}
 }
