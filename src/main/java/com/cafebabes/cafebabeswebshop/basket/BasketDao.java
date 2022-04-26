4a1ba1427deb5b774fdf14952855989754f3e4e0
gkevin94 <gkevin94@gmail.com>
2022-04-26 08:38:23 +0200

    created attribute in BasketDao.java

--- src/main/java/com/cafebabes/cafebabeswebshop/basket/BasketDao.java
+++ src/main/java/com/cafebabes/cafebabeswebshop/basket/BasketDao.java
@@ -1,7 +1,10 @@
 package com.cafebabes.cafebabeswebshop.basket;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class BasketDao {
+
+    private JdbcTemplate jdbcTemplate;
 }
