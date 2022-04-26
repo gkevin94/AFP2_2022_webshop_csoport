fa33139e1ad290c7f299a662b8606d1a3107db25
gkevin94 <gkevin94@gmail.com>
2022-04-26 08:32:23 +0200

    created getters and setters in BasketItem.java

--- src/main/java/com/cafebabes/cafebabeswebshop/basket/BasketItem.java
+++ src/main/java/com/cafebabes/cafebabeswebshop/basket/BasketItem.java
@@ -10,4 +10,60 @@ public class BasketItem {
     private int pieces;
     private String username;
 
+
+    public long getBasketId() {
+        return basketId;
+    }
+
+    public void setBasketId(long basketId) {
+        this.basketId = basketId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(long productId) {
+        this.productId = productId;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getPieces() {
+        return pieces;
+    }
+
+    public void setPieces(int pieces) {
+        this.pieces = pieces;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
