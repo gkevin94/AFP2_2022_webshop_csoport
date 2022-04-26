7c35f07ad80a7061dade9980ca93a49adbc42bcb
gkevin94 <gkevin94@gmail.com>
2022-04-26 08:31:41 +0200

    created attributes in BasketItem.java

--- src/main/java/com/cafebabes/cafebabeswebshop/basket/BasketItem.java
+++ src/main/java/com/cafebabes/cafebabeswebshop/basket/BasketItem.java
@@ -1,4 +1,13 @@
 package com.cafebabes.cafebabeswebshop.basket;
 
 public class BasketItem {
+
+    private long productId;
+    private long basketId;
+    private String name;
+    private String address;
+    private int price;
+    private int pieces;
+    private String username;
+
 }
