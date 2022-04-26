a21a9091bc4040ddf8fa2ddec55c2c6f335ee9d3
Bettembuk Bence <bettembukbence@gmail.com>
2022-04-26 15:21:41 +0200

    Create FeedbackValidator.java

---
+++ src/main/java/com/cafebabes/cafebabeswebshop/feedback/FeedbackValidator.java
@@ -0,0 +1,15 @@
+package com.training360.cafebabeswebshop.feedback;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class FeedbackValidator {
+
+
+    public boolean containsHTMLCode(Feedback feedback){
+        Pattern p = Pattern.compile("<[^>]*>");
+        Matcher m = p.matcher(feedback.getFeedback());
+        return m.find();
+    }
+
+}
