8e83b2c88e3314b1bd03aa18042e86bf7a3c99a8
gkevin94 <gkevin94@gmail.com>
2022-04-30 15:02:49 +0200

    implemented saveBasketItemAndGetId in BasketController.java for "/basket/{address}" endpoint

--- src/main/java/com/cafebabes/cafebabeswebshop/basket/BasketController.java
+++ src/main/java/com/cafebabes/cafebabeswebshop/basket/BasketController.java
@@ -1,5 +1,9 @@
 package com.cafebabes.cafebabeswebshop.basket;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +13,10 @@ public class BasketController {
 
     public BasketController(BasketService basketService) {
         this.basketService = basketService;
+    }
+
+    @PostMapping("/basket/{address}")
+    public long saveBasketItemAndGetId(@PathVariable String address, @RequestBody BasketItem basketItem, Authentication authentication) {
+        return authentication == null ? -1 : basketService.saveBasketItemAndGetId(address, authentication, basketItem);
     }
 }
