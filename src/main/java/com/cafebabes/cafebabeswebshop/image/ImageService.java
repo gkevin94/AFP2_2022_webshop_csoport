2b8f3f00474d16029c804daf84bf731db533c2dc
gkevin94 <gkevin94@gmail.com>
2022-04-30 16:28:21 +0200

    created imageDao attribute an constructor in ImageService.java

--- src/main/java/com/cafebabes/cafebabeswebshop/image/ImageService.java
+++ src/main/java/com/cafebabes/cafebabeswebshop/image/ImageService.java
@@ -4,4 +4,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ImageService {
+
+    private ImageDao imageDao;
+
+    public ImageService(ImageDao imageDao) {
+        this.imageDao = imageDao;
+    }
 }
