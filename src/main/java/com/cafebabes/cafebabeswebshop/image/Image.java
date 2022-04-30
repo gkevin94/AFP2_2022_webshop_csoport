b584c554eec9a509af6e150c8324697945326ac5
gkevin94 <gkevin94@gmail.com>
2022-04-30 16:24:02 +0200

    created 5 attributes in Image.java

--- src/main/java/com/cafebabes/cafebabeswebshop/image/Image.java
+++ src/main/java/com/cafebabes/cafebabeswebshop/image/Image.java
@@ -1,4 +1,14 @@
 package com.cafebabes.cafebabeswebshop.image;
 
+import org.springframework.http.MediaType;
+
 public class Image {
+
+    private long id;
+    private byte[] fileBytes;
+    private MediaType mediaType;
+    private String fileName;
+    private long productId;
+
+
 }
