bdbcdbe4d1c11852eeb42939dcf39a01bd46e00e
gkevin94 <gkevin94@gmail.com>
2022-04-30 15:02:13 +0200

    implemented saveBasketItemAndGetId in BasketService.java

--- src/main/java/com/cafebabes/cafebabeswebshop/basket/BasketService.java
+++ src/main/java/com/cafebabes/cafebabeswebshop/basket/BasketService.java
@@ -1,5 +1,7 @@
 package com.cafebabes.cafebabeswebshop.basket;
 
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,4 +12,19 @@ public class BasketService {
     public BasketService(BasketDao basketDao) {
         this.basketDao = basketDao;
     }
+
+    public long saveBasketItemAndGetId(String address, Authentication authentication, BasketItem basketItem) {
+        basketItem.setUsername(authentication.getName());
+        basketItem.setAddress(address);
+        try {
+            int piecesInBasket = basketDao.getBasketItem(basketItem).getPieces();
+            basketItem.setPieces(basketItem.getPieces() + piecesInBasket);
+            basketDao.updateBasketItemPieces(basketItem);
+            return basketItem.getBasketId();
+        } catch (EmptyResultDataAccessException noResult) {
+            return basketDao.saveBasketItemAndGetId(basketItem);
+        }
+    }
+
+
 }
