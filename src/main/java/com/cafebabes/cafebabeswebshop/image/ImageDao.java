619bfe66e9ee9535f3668849a88c8c6e1976f7d2
gkevin94 <gkevin94@gmail.com>
2022-04-30 16:41:50 +0200

    implemented saveImage in ImageDao.java

--- src/main/java/com/cafebabes/cafebabeswebshop/image/ImageDao.java
+++ src/main/java/com/cafebabes/cafebabeswebshop/image/ImageDao.java
@@ -1,7 +1,16 @@
 package com.cafebabes.cafebabeswebshop.image;
 
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Repository
 public class ImageDao {
@@ -12,5 +21,12 @@ public class ImageDao {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    public void saveImage(Image image) {
+        try {
+            jdbcTemplate.update("INSERT INTO images (image_file, file_type, file_name, product_id) VALUES (?, ?, ?, ?);", image.getFileBytes(), image.getMediaType().toString(), image.getFileName(), image.getProductId());
 
+        } catch (DataAccessException daex) {
+            throw new IllegalArgumentException("Cannot save image", daex);
+        }
+    }
 }
