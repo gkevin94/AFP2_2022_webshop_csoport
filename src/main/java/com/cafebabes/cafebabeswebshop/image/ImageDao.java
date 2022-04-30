616f32083ca6708e659d8f74a9a1a8aaf3f5b793
gkevin94 <gkevin94@gmail.com>
2022-04-30 16:28:41 +0200

    created jdbcTemplate attribute an constructor in ImageDao.java

--- src/main/java/com/cafebabes/cafebabeswebshop/image/ImageDao.java
+++ src/main/java/com/cafebabes/cafebabeswebshop/image/ImageDao.java
@@ -1,9 +1,16 @@
 package com.cafebabes.cafebabeswebshop.image;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ImageDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public ImageDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
 
 }
