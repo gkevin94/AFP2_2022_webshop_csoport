36c4433cc8c28c435f1c2152836683c4366ae85b
gkevin94 <gkevin94@gmail.com>
2022-04-30 16:28:02 +0200

    created imageService attribute an constructor in ImageController.java

--- src/main/java/com/cafebabes/cafebabeswebshop/image/ImageController.java
+++ src/main/java/com/cafebabes/cafebabeswebshop/image/ImageController.java
@@ -4,4 +4,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ImageController {
+
+    private ImageService imageService;
+
+    public ImageController(ImageService imageService) {
+        this.imageService = imageService;
+    }
 }
