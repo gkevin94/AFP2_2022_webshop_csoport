bb1332d8b9e7cff7c079b961ec34898d6f58b562
gkevin94 <gkevin94@gmail.com>
2022-04-30 15:12:30 +0200

    implemented getBasketItems in BasketController.java for "/basket" endpoint

--- src/main/java/com/cafebabes/cafebabeswebshop/basket/BasketController.java
+++ src/main/java/com/cafebabes/cafebabeswebshop/basket/BasketController.java
@@ -1,10 +1,9 @@
 package com.cafebabes.cafebabeswebshop.basket;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class BasketController {
@@ -18,5 +17,10 @@ public class BasketController {
     @PostMapping("/basket/{address}")
     public long saveBasketItemAndGetId(@PathVariable String address, @RequestBody BasketItem basketItem, Authentication authentication) {
         return authentication == null ? -1 : basketService.saveBasketItemAndGetId(address, authentication, basketItem);
+    }
+
+    @GetMapping("/basket")
+    public List<BasketItem> getBasketItems(Authentication authentication) {
+        return authentication == null ? null : basketService.getBasketItems(authentication);
     }
 }
