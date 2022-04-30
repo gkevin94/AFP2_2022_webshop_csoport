f8b2751be0dd50d89bed44db798cb059043b9e19
gkevin94 <gkevin94@gmail.com>
2022-04-30 16:24:33 +0200

    created 2 constructors in Image.java

--- src/main/java/com/cafebabes/cafebabeswebshop/image/Image.java
+++ src/main/java/com/cafebabes/cafebabeswebshop/image/Image.java
@@ -10,5 +10,14 @@ public class Image {
     private String fileName;
     private long productId;
 
+    public Image() {
+    }
 
+    public Image(long id, byte[] fileBytes, MediaType mediaType, String fileName, long productId) {
+        this.id = id;
+        this.fileBytes = fileBytes;
+        this.mediaType = mediaType;
+        this.fileName = fileName;
+        this.productId = productId;
+    }
 }
