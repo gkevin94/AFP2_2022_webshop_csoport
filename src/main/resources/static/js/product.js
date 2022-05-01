93a2ee4840afb880f08f03550518010b2b842841
Szabolcs Veres <73792909+szab0lcs9@users.noreply.github.com>
2022-05-01 22:03:23 +0200

    implemented showFeedbacks function in product.js

--- src/main/resources/static/js/product.js
+++ src/main/resources/static/js/product.js
@@ -110,3 +110,81 @@ function fetchAnotherImage(productId, offset) {
         });
 }
 
+function showFeedbacks(jsonData) {
+
+    var feedbacks = document.getElementById('feedbacks');
+    feedbacks.innerHTML = '';
+
+//  Display the feedback of the registered User
+    var defaultUserFeedback = "";
+
+    if(typeof user != "undefined"){
+
+        var userId = user.id;
+        for(var i = 0; i < jsonData.length; i++){
+            if(jsonData[i].user.id == userId){
+                defaultUserFeedback = jsonData[i].feedback;
+                break;
+            }
+        }
+    }
+    var registeredUsersFeedbackTextArea = document.getElementById("feedback-text");
+    registeredUsersFeedbackTextArea.innerHTML = defaultUserFeedback;
+    console.log(defaultUserFeedback);
+//    ------------------
+
+// Listing feedbacks
+
+    var visibility;
+
+    var star =' <font color="#c59b08">&starf;</font>';
+
+    for (var i = 0; i < jsonData.length; i++) {
+//  Setting the visibility of the delete button
+        star =' <font color="#c59b08">&starf;</font>';
+        star = star.repeat(jsonData[i].rating);
+        visibility = 'hidden';
+        if(jsonData[i].user.id == userId){
+            visibility = 'visible';
+        }
+        feedbacks.innerHTML += `
+      <div class="container">
+          <div>
+              <div id="carousel-feedbacks" class="carousel slide" data-ride="carousel">
+                  <div class="carousel-inner">
+                      <div class="item active">
+                          <div class="col-md-4 col-sm-6">
+                              <div class="block-text rel zmin">
+                              <div class="ad-right" id="feedback-and-delete-buttons">
+                              <button onclick="deleteFeedback(${jsonData[i].id})" id="deleteFeedback#"+${jsonData[i].id} class="btn btn-danger" ${visibility}><i class='fas fa-trash-alt'></i></button>
+                              </div>
+                                  <small class="text-muted">Dátum: ${jsonData[i].feedbackDate.replace('T', ' ')}</small>
+                                  <div class="mark">Értékelés: ${star}<span class="rating-input"><span
+                                          data-value="0"
+                                          class="glyphicon glyphicon-star"></span><span
+                                          data-value="1" class="glyphicon glyphicon-star"></span><span data-value="2"
+                                                                                                       class="glyphicon glyphicon-star"></span><span
+                                          data-value="3" class="glyphicon glyphicon-star"></span><span data-value="4"
+                                                                                                       class="glyphicon glyphicon-star-empty"></span><span
+                                          data-value="5" class="glyphicon glyphicon-star-empty"></span>  </span>
+                                  </div>
+                                  <div id="feedback#${jsonData[i].id}">
+                                  <p id = "fb">${jsonData[i].feedback}</p>
+                                  </div>
+                                  <ins class="ab zmin sprite sprite-i-triangle block"></ins>
+                              </div><br>
+                              <div class="person-text rel" >
+                              <img src="images/member.png"/>
+                                  <span class= "surf medium">${jsonData[i].user.name}</span>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+
+
+                `;
+    }
+}
