87995f85a493c64c1ae73b32866451bca39fc91f
Szabolcs Veres <73792909+szab0lcs9@users.noreply.github.com>
2022-05-02 22:30:47 +0200

    implemented fetchImage function in basket.js

--- src/main/resources/static/js/basket.js
+++ src/main/resources/static/js/basket.js
@@ -12,3 +12,21 @@ function fetchBasket(){
             showBasket(jsonData);
         });
 }
+
+function fetchImage(productId) {
+    var productImage = document.getElementById(`img-${productId}`);
+    console.log(productImage);
+    console.log(productId);
+
+    fetch('/image/' + productId + '/' + 0)
+        .then(function(response) {
+            if(response.status == 200) {
+                return response.blob();
+            }
+            productImage.src = 'https://pbs.twimg.com/profile_images/758084549821730820/_HYHtD8F_400x400.jpg';
+        })
+        .then(function(myBlob) {
+            var objectURL = URL.createObjectURL(myBlob);
+            productImage.src = objectURL;
+        });
+}
