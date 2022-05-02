8a117a4cc8038d24163c5428339a1939473ea850
Tamás Fodor <fodor.tamas48@gmail.com>
2022-05-03 00:00:19 +0200

    implemented showDeliveries in delivery.js

--- src/main/resources/static/js/delivery.js
+++ src/main/resources/static/js/delivery.js
@@ -11,3 +11,14 @@ function fetchUserDeliveries(){
            showdeliveries(jsonData);
         });
 }
+
+function showDeliveries(jsonData){
+    var deliveryDiv = document.querySelector('#deliveryDiv');
+    for (let i = 0; i < jsonData.length; i++) {
+        let div = document.createElement('div');
+        div.setAttribute('class', 'delivery-box', 'd-flex justify-content-center');
+        div.setAttribute('id', `div${i}`);
+        div.innerHTML = `<p>${jsonData[i].deliveryAddress}</p><br><input type='radio' id="input${i}" class='radio-input' name="delivery" >`
+        deliveryDiv.appendChild(div);        
+    }
+}
