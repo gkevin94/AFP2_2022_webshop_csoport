187d1eb52c14700ce3a6df535a221028d0aa6e74
gkevin94 <gkevin94@gmail.com>
2022-05-02 09:07:05 +0200

    created 5 attributes in Dashboard.java

--- src/main/java/com/cafebabes/cafebabeswebshop/dashboard/Dashboard.java
+++ src/main/java/com/cafebabes/cafebabeswebshop/dashboard/Dashboard.java
@@ -1,4 +1,10 @@
 package com.cafebabes.cafebabeswebshop.dashboard;
 
 public class Dashboard {
+
+    private int allUsers;
+    private int allProducts;
+    private int activeProducts;
+    private int allOrders;
+    private int activeOrders;
 }
