e22e4f499842e2dfb2af8e151cebdc99a47b8e11
Szabolcs Veres <73792909+szab0lcs9@users.noreply.github.com>
2022-05-02 22:27:41 +0200

    implemented fetchBasket function in basket.js

---
+++ src/main/resources/static/js/basket.js
@@ -0,0 +1,12 @@
+function fetchBasket(){
+    var url = "/basket";
+    fetch(url)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(jsonData) {
+            var productId = jsonData;
+            console.log(jsonData);
+            showBasket(jsonData);
+        });
+}
