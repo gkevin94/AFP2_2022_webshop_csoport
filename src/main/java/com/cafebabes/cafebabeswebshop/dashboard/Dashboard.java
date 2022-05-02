bd9eda86827980c9f338c19a909bec513153470e
gkevin94 <gkevin94@gmail.com>
2022-05-02 09:08:46 +0200

    created constructor in Dashboard.java

--- src/main/java/com/cafebabes/cafebabeswebshop/dashboard/Dashboard.java
+++ src/main/java/com/cafebabes/cafebabeswebshop/dashboard/Dashboard.java
@@ -7,4 +7,12 @@ public class Dashboard {
     private int activeProducts;
     private int allOrders;
     private int activeOrders;
+
+    public Dashboard(int allUsers, int allProducts, int activeProducts, int allOrders, int activeOrders) {
+        this.allUsers = allUsers;
+        this.allProducts = allProducts;
+        this.activeProducts = activeProducts;
+        this.allOrders = allOrders;
+        this.activeOrders = activeOrders;
+    }
 }
