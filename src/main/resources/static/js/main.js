dc9fd36aebb67454c857fc2aedddb75858cb8186
Tamás Fodor <fodor.tamas48@gmail.com>
2022-05-03 15:51:18 +0200

    implemented size and category dropdown onchange effects in main.js

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,30 @@
+let sizeDropdown = document.querySelector("#size-select");
+let categoryDropdown = document.querySelector("#category-select");
+let url = new URL(document.location);
+
+sizeDropdown.onchange = function(){
+    let size = sizeDropdown[sizeDropdown.selectedIndex].value;
+    if (url.searchParams.get("category"))
+        window.location.href = `/index.html?start=0&size=${size}&category=${url.searchParams.get("category")}`;
+    else
+        window.location.href = `/index.html?start=0&size=${size}`;
+};
+
+categoryDropdown.onchange = function(){
+    if (categoryDropdown[categoryDropdown.selectedIndex].value != 'all') {
+        let category = categoryDropdown[categoryDropdown.selectedIndex].value;
+        window.location.href = `/index.html?category=${category}`;
+    } else
+        window.location.href = `/`;
+};
+
+if(!url.searchParams.get("start") && !url.searchParams.get("category")) {
+    fetchProducts();
+} else if (!url.searchParams.get("category")) {
+    fetchProductsWithStartAndSize();
+} else {
+    fetchProductsWithStartAndSizeAndCategory();
+}
+    fetchAdvice();
+
+sizeDropdown.value = url.searchParams.get("size") || 999;
