fd5b929c9e7e2208f081f9316b3d559971cd96ed
Tamás Fodor <fodor.tamas48@gmail.com>
2022-05-04 13:17:23 +0200

    implemented showTableBody in myorders.js

--- src/main/resources/static/js/myorders.js
+++ src/main/resources/static/js/myorders.js
@@ -11,37 +11,65 @@ function fetchMyOrders(){
     });
 }
 
-var container = document.querySelector("#container");
+let container = document.querySelector("#container");
 container.innerHTML = "";
-var table = document.createElement("table");
+let table = document.createElement("table");
 table.setAttribute('class', 'table table-striped');
 container.appendChild(table);
 
 function showTableHead(jsonData){
     console.log(jsonData);
-    var thead = document.createElement("thead");
+    let thead = document.createElement("thead");
     table.appendChild(thead);
 
-    var trHead = document.createElement("tr");
+    let trHead = document.createElement("tr");
     thead.appendChild(trHead);
 
-    var thDate = document.createElement("th");
+    let thDate = document.createElement("th");
     thDate.setAttribute('scope', 'col');
     thDate.innerHTML = "Dátum";
     trHead.appendChild(thDate);
 
-    var thName = document.createElement("th");
+    let thName = document.createElement("th");
     thName.setAttribute('scope', 'col');
     thName.innerHTML = "Termék";
     trHead.appendChild(thName);
             
-    var thPiece = document.createElement("th");
+    let thPiece = document.createElement("th");
     thName.setAttribute('scope', 'col');
     thPiece.innerHTML = "Darab";
     trHead.appendChild(thPiece);
 
-    var thDelivery = document.createElement("th");
+    let thDelivery = document.createElement("th");
     thName.setAttribute('scope', 'col');
     thDelivery.innerHTML = "Szállítási cím";
     trHead.appendChild(thDelivery);
+}
+
+function showTableBody(jsonData, i, j){
+    let tbody = document.createElement("tbody");
+    table.appendChild(tbody);
+
+    let tr = document.createElement("tr");
+    tbody.appendChild(tr);
+
+    let dateTd = document.createElement("td");
+    dateTd.innerHTML = jsonData[i].purchaseDate;
+    dateTd.setAttribute('id', 'date');
+    tr.appendChild(dateTd);
+
+    let nameTd = document.createElement("td");
+    nameTd.innerHTML = jsonData[i].orderedProducts[j].orderingName;
+    nameTd.setAttribute('id', 'name');
+    tr.appendChild(nameTd);
+
+    let pieceTd = document.createElement("td");
+    pieceTd.innerHTML = jsonData[i].orderedProducts[j].pieces;
+    tr.appendChild(pieceTd);
+
+    let deliveryTd = document.createElement("td");
+    deliveryTd.innerHTML = jsonData[i].delivery.deliveryAddress;
+    tr.appendChild(deliveryTd);
+
+    tbody.appendChild(tr);
 }
