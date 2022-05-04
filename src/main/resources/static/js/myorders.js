9204a23cc7d1cc080d550defe7e46efab0297978
Tamás Fodor <fodor.tamas48@gmail.com>
2022-05-04 13:05:29 +0200

    implemented showTableHead in myorders.js

--- src/main/resources/static/js/myorders.js
+++ src/main/resources/static/js/myorders.js
@@ -10,3 +10,38 @@ function fetchMyOrders(){
         showTable(jsonData)
     });
 }
+
+var container = document.querySelector("#container");
+container.innerHTML = "";
+var table = document.createElement("table");
+table.setAttribute('class', 'table table-striped');
+container.appendChild(table);
+
+function showTableHead(jsonData){
+    console.log(jsonData);
+    var thead = document.createElement("thead");
+    table.appendChild(thead);
+
+    var trHead = document.createElement("tr");
+    thead.appendChild(trHead);
+
+    var thDate = document.createElement("th");
+    thDate.setAttribute('scope', 'col');
+    thDate.innerHTML = "Dátum";
+    trHead.appendChild(thDate);
+
+    var thName = document.createElement("th");
+    thName.setAttribute('scope', 'col');
+    thName.innerHTML = "Termék";
+    trHead.appendChild(thName);
+            
+    var thPiece = document.createElement("th");
+    thName.setAttribute('scope', 'col');
+    thPiece.innerHTML = "Darab";
+    trHead.appendChild(thPiece);
+
+    var thDelivery = document.createElement("th");
+    thName.setAttribute('scope', 'col');
+    thDelivery.innerHTML = "Szállítási cím";
+    trHead.appendChild(thDelivery);
+}
