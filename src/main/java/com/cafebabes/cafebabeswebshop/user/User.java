72f600956dc055017dc5dc41f7ad12a0045c176c
Artur Pamlenyi <artur.pamlenyi@gmail.com>
2022-05-04 20:39:32 +0200

    added user parameters in User.java

--- src/main/java/com/cafebabes/cafebabeswebshop/user/User.java
+++ src/main/java/com/cafebabes/cafebabeswebshop/user/User.java
@@ -43,3 +43,63 @@ public class User {
         this.role = role;
         this.userStatus = userStatus;
     }
+
+    public int getEnabled() {
+        return enabled;
+    }
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getUserStatus() {
+        return userStatus;
+    }
+
+    public void setUserStatus(String userStatus) {
+        this.userStatus = userStatus;
+    }
+}
