80a6c9d8a5eb6d7eb4933cc7320665bc83f2f0ea
Artur Pamlenyi <artur.pamlenyi@gmail.com>
2022-05-05 22:49:57 +0200

    modified Order.java

--- src/main/java/com/cafebabes/cafebabeswebshop/order/Order.java
+++ src/main/java/com/cafebabes/cafebabeswebshop/order/Order.java
@@ -1,5 +1,38 @@
 package com.cafebabes.cafebabeswebshop.order;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class Order {
+
+    private long id;
+    private LocalDateTime purchaseDate;
+    private long userId;
+    private long total;
+    private long sumQuantity;
+    private OrderStatus orderStatus;
+    private com.cafebabes.cafebabeswebshop.delivery.Delivery delivery;
+    private List<OrderedProduct> orderedProducts;
+
+    public Order() {
+    }
+
+    public Order(long id, long userId, long total, long sumQuantity, String status) {
+        this(id, LocalDateTime.now(), userId, total, sumQuantity, status);
+    }
+
+    public Order(long id, LocalDateTime purchaseDate, long userId, long total, long sumQuantity, String status) {
+        this.id = id;
+        this.purchaseDate = purchaseDate;
+        this.userId = userId;
+        this.total = total;
+        this.sumQuantity = sumQuantity;
+        this.orderStatus = OrderStatus.valueOf(status);
+    }
+
+    public Order(long id, LocalDateTime purchaseDate, long userId, long total, long sumQuantity, String orderStatus, com.cafebabes.cafebabeswebshop.delivery.Delivery delivery) {
+        this(id, purchaseDate, userId, total, sumQuantity, orderStatus);
+        this.delivery = delivery;
+    }
 
 }
