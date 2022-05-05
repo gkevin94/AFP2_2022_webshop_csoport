9d954ca481107d3790e3b0a029cb1b369e1e77d6
Artur Pamlenyi <artur.pamlenyi@gmail.com>
2022-05-05 23:01:03 +0200

    implemented getmapping in OrderController.java

--- src/main/java/com/cafebabes/cafebabeswebshop/order/OrderController.java
+++ src/main/java/com/cafebabes/cafebabeswebshop/order/OrderController.java
@@ -1,9 +1,10 @@
 package com.cafebabes.cafebabeswebshop.order;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class OrderController {
@@ -24,5 +25,60 @@ public class OrderController {
             return new ResultStatus(ResultStatusEnum.NOT_OK, e.getMessage());
         }
     }
+
+    @GetMapping("/myorders")
+    public List<Order> listMyOrders(Authentication authentication) {
+        return orderService.listMyOrders(authentication);
+    }
+
+    @GetMapping("/orders")
+    public List<Order> listAllOrders() {
+        return orderService.listAllOrders();
+    }
+
+    @GetMapping("/orders/{id}")
+    public List<OrderedProduct> listOrderedProductsByOrderId(@PathVariable long id) {
+        return orderService.listOrderedProductsByOrderId(id);
+    }
+
+    @PostMapping("/orders/{id}")
+    public ResultStatus deleteOrder(@PathVariable long id) {
+        if (validator.isValidOrderId(id)) {
+            orderService.deleteOrder(id);
+            return new ResultStatus(ResultStatusEnum.OK, String.format("Order successfully deleted with id %d", id));
+        } else {
+            return new ResultStatus(ResultStatusEnum.NOT_OK, "Invalid id");
+        }
+    }
+
+    @DeleteMapping("/orders/{id}/{address}")
+    public ResultStatus deleteOneItemFromOrder(@PathVariable long id, @PathVariable String address) {
+        try {
+            if (validator.isExistingOrderId(id) && validator.isExistingProductAddress(address)) {
+                orderService.deleteOneItemFromOrder(id, address);
+                return new ResultStatus(ResultStatusEnum.OK, "Ordered product deleted successfully");
+            } else {
+                return new ResultStatus(ResultStatusEnum.NOT_OK, "Invalid id, or address");
+            }
+        } catch (DataAccessException sql) {
+            return new ResultStatus(ResultStatusEnum.NOT_OK, "Invalid id, or address");
+        }
+    }
+
+    @PostMapping("/orders/{id}/{status}")
+    public ResultStatus updateOrderStatus(@PathVariable long id, @PathVariable String status) {
+        if (validator.isValidStatus(status.toUpperCase()) && validator.isValidOrderId(id)) {
+            orderService.updateOrderStatus(id, status.toUpperCase());
+            return new ResultStatus(ResultStatusEnum.OK, String.format("Order status successfully updated with id %d", id));
+        } else {
+            return new ResultStatus(ResultStatusEnum.NOT_OK, "Invalid id or status");
+        }
+    }
+
+    @PostMapping("/orders/piece")
+    public void updateOrderedProductPiece(@RequestBody OrderedProduct op) {
+        orderService.updateOrderedProductPiece(op);
+    }
+
 
 }
