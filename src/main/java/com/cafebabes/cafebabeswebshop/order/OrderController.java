eaa67806a784b027b4deeadd0afa60bf310b8fc4
Artur Pamlenyi <artur.pamlenyi@gmail.com>
2022-05-05 23:00:20 +0200

    implemented postmapping in OrderController.java

--- src/main/java/com/cafebabes/cafebabeswebshop/order/OrderController.java
+++ src/main/java/com/cafebabes/cafebabeswebshop/order/OrderController.java
@@ -1,4 +1,28 @@
 package com.cafebabes.cafebabeswebshop.order;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class OrderController {
+    private OrderService orderService;
+    private OrderValidator validator;
+
+    public OrderController(OrderService orderService, UserService userService, ProductService productService) {
+        this.orderService = orderService;
+        this.validator = new OrderValidator(orderService, userService, productService);
+    }
+
+    @PostMapping("/myorders")
+    public ResultStatus saveOrderAndGetId(Authentication authentication, @RequestBody Delivery delivery) {
+        try {
+            long id = orderService.saveOrderAndGetId(authentication, delivery);
+            return new ResultStatus(ResultStatusEnum.OK, String.format("Order successfully created with id %d", id));
+        } catch (IllegalStateException | IllegalArgumentException e) {
+            return new ResultStatus(ResultStatusEnum.NOT_OK, e.getMessage());
+        }
+    }
+
 }
