cbcab40bfc2d76228e9f31a5599ea24c71b85c8b
Szabolcs Veres <73792909+szab0lcs9@users.noreply.github.com>
2022-05-05 23:55:39 +0200

    implemented startUpload function in uploads.js

--- src/main/resources/static/js/upload.js
+++ src/main/resources/static/js/upload.js
@@ -12,3 +12,22 @@ function init() {
         document.querySelector("[for='"+ this.id +"']").innerText = this.files[0].name;
     })
 }
+
+function startUpload() {
+    let file = document.getElementById('fileToUpload').files[0];
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('productId', productId);
+
+    fetch('/image', {
+        method: 'POST',
+        body: formData
+    }).then(response => {
+        return response.text();
+    }).then(message => {
+        alert(message);
+        document.getElementById('uploadForm').reset();
+        let messageNode = document.getElementById('messageP');
+        fetchImage(productId, 0);
+    });
+}
