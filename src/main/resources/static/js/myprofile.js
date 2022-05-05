3e09c401ccf14bb9927cdcb04dd2809adb69d457
Tamás Fodor <fodor.tamas48@gmail.com>
2022-05-05 12:42:25 +0200

    implemented handleFormSubmit in myprofile.js

--- src/main/resources/static/js/myprofile.js
+++ src/main/resources/static/js/myprofile.js
@@ -19,3 +19,47 @@ function fetchUser() {
       name = jsonData.name;
     });
 }
+
+function handleFormSubmit() {
+    let div = document.querySelector('#response-box');
+    let form = document.forms["form-register"];
+    if (form.confirm.value != form.password.value) {
+        div.setAttribute("class", "alert alert-danger");
+        div.innerHTML = "A két jelszó nem egyezik!";
+        return;
+    }
+    let request = {
+                    "name": name,
+                    "email": "email",
+                    "userName": form.username.value,
+                    "password": form.password.value,
+                    "enabled": 1,
+                    "role": "ROLE_USER",
+                    "userStatus": "ACTIVE"
+                }
+    console.log(request);
+    fetch("/users/" + id, {
+        method: "POST",
+        body: JSON.stringify(request),
+        headers: {
+            "Content-type": "application/json"
+        }
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(json) {
+        console.log(json);
+        if(json.status == "OK") {
+            div.setAttribute("class", "alert alert-success");
+            div.innerHTML = "Sikeres módosítás! Átirányítunk...";
+            setTimeout (function() {
+                window.location.href = "/login.html";
+            }, 1000);
+        } else {
+            div.innerHTML = json.message;
+            div.setAttribute("class", "alert alert-danger");
+        }
+    });
+    return false;
+}
