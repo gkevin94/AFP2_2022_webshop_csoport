5f59b974c1a2fc8a2278a37a82c78ec45ac9c427
Szabolcs Veres <73792909+szab0lcs9@users.noreply.github.com>
2022-05-06 00:01:52 +0200

    implemented fetchImage function in uploads.js

--- src/main/resources/static/js/upload.js
+++ src/main/resources/static/js/upload.js
@@ -31,3 +31,38 @@ function startUpload() {
         fetchImage(productId, 0);
     });
 }
+
+function fetchImage(productId, offset) {
+    if(offset == 0)
+        imageContainer.innerHTML = "";
+    fetch('/image/' + productId + '/' + offset)
+        .then(function(response) {
+            if(response.status == 200) {
+                imageContainer.innerHTML += `<div class="col-md-4">
+                                          <div class="card mb-4 box-shadow">
+                                              <img class="card-img-top" id='img-${offset}'
+                                                   alt="surfboard image">
+                                              <div class="card-body">
+                                                  <p class="card-text">${response.headers.get("Content-Disposition").split("=")[1]}</p>
+                                                  <small class="text-muted" id="file-type-${offset}">${response.headers.get("Content-Type")}</small>
+                                                  <p><small class="text-muted" id="file-size-${offset}"></small></p>
+                                                  <div class="d-flex justify-content-between align-items-center">
+                                                      <div class="btn-group">
+                                                          <button type="button" class="btn btn-lm btn-outline-secondary" onclick="deleteImage(${productId}, ${offset})">Törlés</button>
+                                                      </div>
+                                                  </div>
+                                              </div>
+                                          </div>
+                                      </div>`;
+
+                return response.blob();
+            }
+        })
+        .then(function(blob) {
+            if(blob) {
+                document.querySelector(`#file-size-${offset}`).innerHTML = Math.round(blob.size/1024 * 100) / 100 +" kB";
+                document.querySelector(`#img-${offset}`).src = URL.createObjectURL(blob);
+                fetchImage(productId, offset+1);
+            }
+        });
+}
