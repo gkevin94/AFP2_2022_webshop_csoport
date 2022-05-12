102db2daa004ba712096fdcc9721a458f5ab2021
Artur Pamlenyi <artur.pamlenyi@gmail.com>
2022-05-12 22:00:46 +0200

    implemented Component in CategoryValidator.java

--- src/main/java/com/cafebabes/cafebabeswebshop/category/CategoryValidator.java
+++ src/main/java/com/cafebabes/cafebabeswebshop/category/CategoryValidator.java
@@ -1,4 +1,21 @@
 package com.cafebabes.cafebabeswebshop.category;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class CategoryValidator {
+    private CategoryDao categoryDao;
+
+    public CategoryValidator(CategoryDao categoryDao) {
+        this.categoryDao = categoryDao;
+    }
+
+    public boolean isEmpty(String str) {
+        return str == null || str.trim().isEmpty();
+    }
+
+    public boolean isExistingCategoryName(Category category) {
+        return !categoryDao.getCategoryByName(category.getName()).isEmpty();
+    }
 }
+
